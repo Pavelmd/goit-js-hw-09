4239f40cb2f5f3b76de82a719de6dba58057058b
Pavelmd <pashanovosvit2@gmail.com>
2023-09-13 23:50:15 +0300

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  const buttonStart = document.querySelector('[data-start]');
+  const buttonStop = document.querySelector('[data-stop]');
+  buttonStop.disabled = false;
+  let timerId = null;
 
+
+  buttonStart.addEventListener('click', () => {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+  
+    timerId = setInterval(() => {
+      document.body.style.background = getRandomHexColor();
+    }, 1000);
+  });
+  
+  buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+  });
